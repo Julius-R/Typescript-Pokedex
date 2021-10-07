10bdaca37f2e91c1cf468c7f3701cab63ba22543
Julius Robinson <julius.robinson95@yahoo.com>
2021-10-06 18:38:39 -0600

    added charts.js

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,66 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {useState, useEffect} from 'react';
+import { Bar } from 'react-chartjs-2';
+import {Pokemon} from './utils/types'
 import './App.css';
 
-function App() {
+
+const App = () => {
+  const [pokemon, setPokemon] = useState<Pokemon>({} as Pokemon);
+  const [data, setData] = useState({} as any);
+  const fetchPokemon = async (query: number | string) => {
+    const res = await fetch(`https://pokeapi.co/api/v2/pokemon/${query}`);
+    const {weight, types, sprites, name, id, height, stats} = await res.json();
+    const dataSet = {
+       labels: ['Hp', 'Attack', 'Defense', 'Sp. Attack', 'Sp. Defense', 'Speed'],
+  datasets: [
+    {
+      label: 'Pokemon Stats',
+      data: stats.map((stat: any) => stat.base_stat),
+      backgroundColor: [
+        'rgba(255, 99, 132, 0.2)',
+        'rgba(54, 162, 235, 0.2)',
+        'rgba(255, 206, 86, 0.2)',
+        'rgba(75, 192, 192, 0.2)',
+        'rgba(153, 102, 255, 0.2)',
+        'rgba(255, 159, 64, 0.2)',
+      ],
+      borderColor: [
+        'rgba(255, 99, 132, 1)',
+        'rgba(54, 162, 235, 1)',
+        'rgba(255, 206, 86, 1)',
+        'rgba(75, 192, 192, 1)',
+        'rgba(153, 102, 255, 1)',
+        'rgba(255, 159, 64, 1)',
+      ],
+      borderWidth: 1,
+    },
+  ],
+    }
+    setData(dataSet)
+    setPokemon({weight, types, sprites, name, id, height, stats});
+  }
+
+  const nextPokemon = () => {
+    pokemon.id === 898 ? fetchPokemon(1) : fetchPokemon(pokemon.id + 1);
+  }
+  const previousPokemon = () => {
+    pokemon.id === 1 ? fetchPokemon(898) : fetchPokemon(pokemon.id - 1);
+  }
+
+  useEffect(() => {
+    fetchPokemon(1);
+  }, [])
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <p>{pokemon.name}</p>
+      {/* <img src={pokemon.sprites.front_default} alt={`default pic of ${pokemon.name}`}/> */}
+      <Bar data={data} width={20}
+	height={10}/>
+      <button onClick={(): void => {
+        console.log(pokemon.sprites.front_default)
+      }}>Click</button>
+      <button onClick={previousPokemon}>Prev</button>
+      <button onClick={nextPokemon}>Next</button>
     </div>
   );
 }
---
+++ src/utils/types.ts
@@ -0,0 +1,32 @@
+
+
+type sprites = {
+    front_default: string;
+    back_default: string;
+    front_shiny: string;
+    back_shiny: string;
+};
+
+type pokeType = {
+    name: string;
+    url: string;
+}
+
+type stat = {
+    base_stat: number;
+    effort: number;
+    stat: {
+        name: string;
+        url: string;
+    };
+}
+
+export interface Pokemon {
+    weight: number;
+    types: pokeType[];
+    sprites: sprites;
+    name: string;
+    id: number;
+    height: number;
+    stats: stat[];
+}
